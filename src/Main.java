658564340b4e460a130122bfcf640fb64da12311
Mark Owings <owingsmk@mail.uc.edu>
2023-02-02 19:20:58 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        // Project init
+        int intOperandA = 10:
+        int intOperandB = 12:
+        int intOperandC = 14:
+        int intSum = 16
+        int intDifference = 18
+        int intQuotient = 20
+        int intModulo = 24
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmentic operator +
+        System.out.println(“The Sum using ints of " + intOperandA + "" + intOperaB + " is " + intSum)"; //Output Statement
+
+    }
+}
